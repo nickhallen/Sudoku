35e82147f5c1a0e9dd7f085e00a88e45c92a6398
somename <someemail>
2015-10-01 12:49:46 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -5,14 +5,15 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
+	SudokuVerifier sudoku=new SudokuVerifier();
 	
 	String correctString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293"; 
 	String incorrectString= "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	String tooLongString= "1234567899123456788912345677891234566789123455678912344567891233456789122345678911234";
-
+    String notOnlyDigitsString = "ABD417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	@Test
 	public void test81StringsCorrectString() {
-		SudokuVerifier sudoku=new SudokuVerifier();
+		
 		int test= sudoku.ruleLength(correctString);
 		assertEquals (test, test=0);
 		
@@ -20,9 +21,21 @@ public class TestSudokuVerifier {
 
 	@Test
 	public void test81StringsInCorrectString() {
-		SudokuVerifier sudoku=new SudokuVerifier();
+		
 		int test= sudoku.ruleLength(tooLongString);
-		assertEquals (test, test=1);
+		assertEquals (test, test=-5);
+		
+	}
+	@Test
+	public void testRule1Results0() {
+		int test= sudoku.rule1(correctString);
+		assertEquals(test, test=0);
+		
+	}
+	@Test
+	public void testRule1ResultsMinus1() {
+		int test= sudoku.rule1(notOnlyDigitsString);
+		assertEquals(test, test=-1);
 		
 	}
 }
