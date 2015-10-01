e5e83eb0f6fc08d375649e82cd22f04b987608dc
somename <someemail>
2015-10-01 12:29:22 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -3,6 +3,16 @@ package org.univoulu.tol.sqatlab.sudoku;
 public class SudokuVerifier {
 
 	public int verify(String candidateSolution) {
+		String sudoku = candidateSolution;
+		if (sudoku.length() !=81)
+			return -5; 
+		
+	}   private int rule1 (String sudoku){
+		for ( int i = 0; i < sudoku.length(); i++)
+			if(!Character.isDigit(sudoku.charAt(i)))
+				return -1;
+	} {
+		
 		// returns 1 if the candidate solution is correct
 		return 0;
 	}
